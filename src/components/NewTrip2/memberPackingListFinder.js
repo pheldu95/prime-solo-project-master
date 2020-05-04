6ce77570f03624355576a00872257f656f8a61fc
Duncan Phelps <phelps.duncan@gmail.com>
2020-05-04 18:30:26 -0500

    got rid of hard coded personal packing list arrays.

--- src/components/NewTrip2/memberPackingListFinder.js
+++ src/components/NewTrip2/memberPackingListFinder.js
@@ -1,19 +1,81 @@
 //this function will find the member packing list we need 
 //based on how many days the trip is
 
-//require item arrays. these will be posted based on how long the trip is.
-let fourDays = require('../ItemArrays/fourDays');
-let eightDays = require('../ItemArrays/eightDays');
-let twelveDays = require('../ItemArrays/twelveDays');
-
 function memberPackingListFinder(days) {
-    if(days<5){
-        return fourDays;
-    } else if (days>4 && days<9) {//array of items if days are less than 9
-        return eightDays;
-    } else if (days > 8 && days < 13){
-        return twelveDays;
-    }
+    let itemArray = [
+
+        {
+            name: 'Pairs of wool socks',
+            quantity: 4
+        },
+        {
+            name: 'sleeping bag',
+            quantity: 1
+        },
+        {
+            name: 'sleeping pad',
+            quantity: 1
+        },
+        {
+            name: 'toiletries',
+            quantity: 1
+        },
+        {
+            name: 'warm layer',
+            quantity: 1
+        },
+        {
+            name: 'head lamp',
+            quantity: 1
+        },
+        {
+            name: 'shirts',
+            quantity: 3
+        },
+        {
+            name: 'baseball cap',
+            quantity: 1
+        },
+        {
+            name: 'pants',
+            quantity: 1
+        },
+        {
+            name: 'shorts',
+            quantity: 1
+        },
+        {
+            name: 'eating utensil',
+            quantity: 1
+        },
+        {
+            name: 'bowl',
+            quantity: 1
+        },
+        {
+            name: 'sunglasses',
+            quantity: 1
+        },
+        {
+            name: 'Camp shoes/flip-flops',
+            quantity: 1
+        },
+        {
+            name: 'rain jacket',
+            quantity: 1
+        },
+        {
+            name: 'Warm hat',
+            quantity: 1
+        },
+        {
+            name: 'Long underwear (if the weather will be cold)',
+            quantity: 1
+        },
+
+
+    ]
+    return itemArray;
 }
 
 module.exports = memberPackingListFinder;
